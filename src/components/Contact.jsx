13c8343d2a6fa85aa256fb08661b5a9c8e012e5a
Nestorq <ndqa96@gmail.com>
2023-12-03 00:16:38 -0400

    fixing form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,27 @@
-import { useRef } from 'react'
-import emailjs from '@emailjs/browser';
+import { useForm } from "react-hook-form";
+// import emailjs from '@emailjs/browser';
 import MailImg from '../assets/MailImg';
 
 function Contact () {
-    const form = useRef();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    //     const serviceId = "service_404lg0h";
+    //     const templateId = "template_1f6qhwr";
 
-        const serviceId = "service_404lg0h";
-        const templateId = "template_1f6qhwr";
+    //     const apiKey = "MaKV5Mj_XzF-ue66j";
 
-        const apiKey = "MaKV5Mj_XzF-ue66j";
-
-        emailjs.sendForm
-        emailjs.sendForm(serviceId, templateId, form.current, apiKey).then(result => console.log(result.text)).catch(e => console.log(e))
-    }
+    //     emailjs.sendForm
+    //     emailjs.sendForm(serviceId, templateId, form.current, apiKey).then(result => console.log(result.text)).catch(e => console.log(e))
+    // }
     return (
         <>
             <section className='ContactSection' data-bs-spy="scroll" id="scrollspyHeading5">
@@ -24,21 +30,151 @@ function Contact () {
                     <div className='ContactImage'>
                         <MailImg />
                     </div>
-                    <div>
-                        <form ref={form} onSubmit={handleSubmit}>
+                    <div className='formContainer'>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <fieldset>
-                                <div className='FormName'>
-                                    <input name='userName' type="text" placeholder='Nombre' required />
-                                    <input name='userLastname' type="text" placeholder='Apellido' required />
+                                <div className="form__group field">
+                                    <input 
+                                        type="input" 
+                                        className="form__field" 
+                                        placeholder="Nombre" 
+                                        {...register("userName", { 
+                                            required: true, 
+                                            maxLength: 20,
+                                        })} 
+                                    />
+                                    <label htmlFor="name" className="form__label">Nombre</label>
                                 </div>
-                                <div className='FormEmail'>
-                                    <input name='email' type='email' id="email" placeholder='Email' required />
+                                {
+                                    errors?.userName?.type === 'required' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        Este campo es requerido.
+                                    </span>
+                                }
+                                {
+                                    errors?.userName?.type === 'maxLength' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        No se admite mas de 20 caracteres.
+                                    </span>
+                                }
+                                <div className="form__group field">
+                                    <input 
+                                        className="form__field" 
+                                        type="text" 
+                                        placeholder='Apellido' 
+                                        {...register("userLastname", { 
+                                            required: true, 
+                                            maxLength: 20,
+                                        })} 
+                                        />
+                                    <label htmlFor="userLastname" className="form__label">Apellido</label>
                                 </div>
+                                {
+                                    errors?.userLastname?.type === 'required' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        Este campo es requerido.
+                                    </span>
+                                }
+                                {
+                                    errors?.userLastname?.type === 'maxLength' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        No se admite mas de 20 caracteres.
+                                    </span>
+                                }
+                                <div className="form__group field">
+                                    <input 
+                                        className="form__field" 
+                                        placeholder='Email' 
+                                        {...register("email", { 
+                                            required: true, 
+                                            pattern: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
+                                        })}                                        
+                                        />
+                                    <label htmlFor="email" className="form__label">Email</label>
+                                </div>
+                                {
+                                    errors?.email?.type === 'required' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        Este campo es requerido.
+                                    </span>
+                                }
+                                {
+                                    errors?.email?.type === 'pattern' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        El correo electronico ingresado es invalido.
+                                    </span>
+                                }
+                                <div className="form__group field">
+                                    <textarea 
+                                        className="form__field" 
+                                        maxLength={500} 
+                                        placeholder='Escribe tu mensaje' 
+                                        {...register("form", { 
+                                            required: true, 
+                                            maxLength: 600,
+                                            minLength: 100
+                                        })} 
+                                        />
+                                    <label htmlFor="form" className="form__label">¡Contáctanos!</label>
+                                </div>
+                                {
+                                    errors?.form?.type === 'required' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        Este campo es requerido.
+                                    </span>
+                                }
+                                {
+                                    errors?.form?.type === 'maxLength' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        No se admite mas de 600 caracteres.
+                                    </span>
+                                }
+                                {
+                                    errors?.form?.type === 'minLength' && 
+                                    <span style={{
+                                        color: 'red',
+                                        fontStyle: 'italic',
+                                        fontWeight: '900'
+                                    }}>
+                                        Debes ingresar mas de 100 caracteres.
+                                    </span>
+                                }
                                 <div>
-                                    <textarea maxLength={500} placeholder='Escribe tu mensaje' name='message'  />
                                 </div>
                                 <div className='FormButton'>
-                                    <button>Enviar</button>
+                                    <button type='submit'>Enviar</button>
                                 </div>
                             </fieldset>
                         </form>
